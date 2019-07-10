0772cabe1dd618c94a4cb023cc5b063804d97961
Karlos Santos <lgvichy@gmail.com>
2019-07-09 19:39:28 -0700

    Testing pure-ECS spawner. Cannot spawn even basic mesh?...

---
+++ Assets/Developers/Lab/Systems/MovementSystem.cs
@@ -0,0 +1,65 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+using Random = Unity.Mathematics.Random;
+
+namespace JanusVagabond
+{
+  public class MovementSystem : ComponentSystem
+  {
+    protected override void OnUpdate()
+    {
+
+      // If has MoveLeft, move it toward the left
+      Entities.WithAllReadOnly<WanderComponent, MoveLeftComponent>().ForEach(
+          (Entity id, ref Translation translation) =>
+          {
+            var deltaTime = Time.deltaTime;
+            translation = new Translation()
+            {
+              Value = new float3(translation.Value.x - deltaTime, translation.Value.y, translation.Value.z)
+            };
+
+            // Components can only be added or removed using PostUpdateCommands.
+            if (translation.Value.x < -10.0f)
+              PostUpdateCommands.RemoveComponent<MoveLeftComponent>(id);
+          }
+      );
+
+      // If has MoveLeft, move it toward the left
+      Entities.WithAllReadOnly<WanderComponent, MoveRightComponent>().ForEach(
+          (Entity id, ref Translation translation) =>
+          {
+            var deltaTime = Time.deltaTime;
+            translation = new Translation()
+            {
+              Value = new float3(translation.Value.x + deltaTime, translation.Value.y, translation.Value.z)
+            };
+
+            // Components can only be added or removed using PostUpdateCommands.
+            if (translation.Value.x > 10.0f)
+              PostUpdateCommands.RemoveComponent<MoveLeftComponent>(id);
+          }
+      );
+
+      // If componenent has wandering but no move tag, give it a random one.
+      Entities.WithAllReadOnly<WanderComponent>().WithNone<MoveLeftComponent>().WithNone<MoveRightComponent>().ForEach(
+          (Entity id, ref Translation translation) =>
+          {
+            var random = new Random(1);
+
+            if (random.NextBool())
+            {
+              PostUpdateCommands.AddComponent(id, new MoveLeftComponent());
+            }
+            else
+            {
+              PostUpdateCommands.AddComponent(id, new MoveRightComponent());
+            }
+          }
+      );
+    }
+  }
+}
---
+++ Assets/Developers/Lab/Systems/SpawnSystem.cs
@@ -0,0 +1,70 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+using UnityEngine;
+
+namespace JanusVagabond
+{
+  [UpdateInGroup(typeof(SimulationSystemGroup))]
+  public class SpawnSystem : JobComponentSystem
+  {
+    // This declares a new kind of job, which is a unit of work to do.
+    // The job is declared as an IJobForEach<Translation, Rotation>,
+    // meaning it will process all entities in the world that have both
+    // Translation and Rotation components. Change it to process the component
+    // types you want.
+    //
+    // The job is also tagged with the BurstCompile attribute, which means
+    // that the Burst compiler will optimize it for the best performance.
+    BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+    protected override void OnCreate()
+    {
+      // Cache the BeginInitializationEntityCommandBufferSystem in a field, so we don't have to create it every frame
+      m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+
+    // [BurstCompile]
+    struct SpawnJob : IJobForEachWithEntity<SpawnDataComponent, LocalToWorld>
+    {
+      // Add fields here that your job needs to do its work.
+      // For example,
+      //    public float deltaTime;
+
+      public EntityCommandBuffer.Concurrent CommandBuffer;
+
+      public void Execute(Entity entity, int index, [ReadOnly] ref SpawnDataComponent spawnData,
+          [ReadOnly] ref LocalToWorld location)
+      {
+        for (var i = 0; i < spawnData.Count; i++)
+        {
+          var instance = CommandBuffer.Instantiate(index, spawnData.Prefab);
+
+          var position = math.transform(location.Value, new float3(i * 1.3F, noise.cnoise(new float2(1, 0) * 0.21F) * 2, 1));
+
+          CommandBuffer.SetComponent(index, instance, new Translation { Value = position });
+        }
+
+        CommandBuffer.DestroyEntity(index, entity);
+      }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+      // Schedule job run:
+      var job = new SpawnJob
+      {
+        CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent()
+      }.Schedule(this, inputDependencies);
+
+      // Add handle to play back the commands up the chain.
+      m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+
+      return job;
+    }
+  }
+}
